6dfe2cade0f7c0076b111276739dbb98dd38a19b
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-11-16 01:37:55 +0900

    change: reconnect websocket added

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,14 +3,18 @@ package main
 import (
 	"github.com/bmizerany/pat"
 	"net/http"
-	handlers "ws/cmd/internal"
+	"ws/cmd/internal"
 )
 
+// routes defines the application routes
 func routes() http.Handler {
 	mux := pat.New()
 
 	mux.Get("/", http.HandlerFunc(handlers.Home))
 	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Get("/static/", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
