996931cfd5cf97f22dc76fd34cdf8a972cfeffbc
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-11-16 00:17:31 +0900

    feat: chat message canbe displayed in client

--- cmd/internal/handlers.go
+++ cmd/internal/handlers.go
@@ -117,6 +117,10 @@ func ListenToWsChannel() {
 			users := getUserList()
 			response.ConnectedUsers = users
 			broadcastToAll(response)
+		case "broadcast":
+			response.Action = "broadcast"
+			response.Message = fmt.Sprintf("<strong>%s</string>: %s", e.Username, e.Message)
+			broadcastToAll(response)
 		}
 
 		//response.Action = "Got here"
